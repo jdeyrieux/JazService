75aec67a83cda081ed2983ec865a177a36112c71
jdeyrieux <jdeyrieux@hotmail.fr>
2014-11-03 16:06:13 +0100

    Update exe and params
    Add instructions to run in tray and add memcached menu instructions

--- JazService.dpr
+++ JazService.dpr
@@ -2,6 +2,8 @@ program JazService;
 
 uses
   Vcl.Forms,
+  SysUtils,
+  Windows,
   fJzAbout in 'fJzAbout.pas' {AboutDiag},
   fJzHtmlWindow in 'fJzHtmlWindow.pas' {HtmlWindow},
   fJzMain in 'fJzMain.pas' {MainForm},
@@ -14,9 +16,19 @@ uses
 {$R *.res}
 
 begin
+  { Do some command-line parameter handling first }
+  if FindCmdLineSwitch('quit', ['-'], True) then
+  begin
+    KillInstance(FindCmdSwitch('-id='));
+    Exit;
+  end;
+
+  { Start it up! }
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.Title := 'JazService';
+  Application.MainFormOnTaskbar := False;
+  Application.Title := 'JazService Tray Menu';
   Application.CreateForm(TMainForm, MainForm);
+  Application.ShowMainForm := False;
+  ShowWindow(Application.Handle, SW_HIDE);
   Application.Run;
 end.
